669e55f9dda4e8973d1ca1c8746e2e2bcb833590
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-28 20:57:44 +0900

    initial commit and tokenizer

---
+++ token.test.ts
@@ -0,0 +1,40 @@
+import { expect, test } from "bun:test";
+import { tokenize, correctlyTokenize } from "./token";
+
+test("numbers", () => {
+  const house = {
+    chair: 2,
+    people: 10,
+  };
+  const raw = JSON.stringify(house);
+  expect(tokenize(raw)).toEqual(correctlyTokenize(raw));
+});
+
+test("number and string", () => {
+  const house = {
+    chair: 2,
+    people: "me",
+  };
+  const raw = JSON.stringify(house);
+  expect(tokenize(raw)).toEqual(correctlyTokenize(raw));
+});
+
+test("array", () => {
+  const house = {
+    chair: 2,
+    people: ["me", "ghost"],
+  };
+  const raw = JSON.stringify(house);
+  expect(tokenize(raw)).toEqual(correctlyTokenize(raw));
+});
+
+test("nested object", () => {
+  const house = {
+    chair: 2,
+    people: {
+      me: 1,
+    },
+  };
+  const raw = JSON.stringify(house);
+  expect(tokenize(raw)).toEqual(correctlyTokenize(raw));
+});
---
+++ token.ts
@@ -0,0 +1,61 @@
+import jsTokens from "js-tokens";
+
+export function correctlyTokenize(raw: string): string[] {
+  const ans = [];
+  for (const token of jsTokens(raw)) {
+    ans.push(token.value);
+  }
+  return ans;
+}
+
+const house = {
+  table: 2,
+  piano: 1,
+  kitchen: 1,
+  chair: 10,
+};
+
+const raw = JSON.stringify(house);
+
+export function tokenize(raw: string): string[] {
+  let buffer = "";
+  let tokens = [];
+
+  for (let cursor = 0; cursor < raw.length; cursor++) {
+    const current = raw.charAt(cursor);
+    switch (current) {
+      case "{":
+        tokens.push(current);
+        break;
+      case "}":
+        tokens.push(buffer);
+        buffer = current;
+        break;
+      case "[":
+        tokens.push(current);
+        break;
+      case "]":
+        tokens.push(buffer);
+        buffer = current;
+        break;
+      case ":":
+        tokens.push(buffer);
+        tokens.push(current);
+        buffer = "";
+        break;
+      case ",":
+        tokens.push(buffer);
+        tokens.push(current);
+        buffer = "";
+        break;
+      default:
+        buffer += current;
+    }
+  }
+  tokens.push("}");
+
+  return tokens;
+}
+
+console.log(tokenize(raw));
+console.log(correctlyTokenize(raw));
